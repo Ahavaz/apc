45c5a807b796a539c8d8aa4281aab208fe9520f6
Lucas César <Ahavaz@users.noreply.github.com>
2016-05-29 12:32:33 -0300

    Update main.c

--- listas/strings/01/main.c
+++ listas/strings/01/main.c
@@ -1,23 +1,29 @@
-﻿/* Escreva um programa que recebe 3 strings com tamanho 20 e imprime-as em ordem crescente */
+/*
+ ======================================================================================================================
+ Crie um programa que lê 3 strings de tamanho 20 e imprime essas strings em ordem alfabética.
+ ======================================================================================================================
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
+	setvbuf(stdout, NULL, _IONBF, 0); // comando para imprimir a saída corretamente no console interno do eclipse
+
 	int n = 3, l = 21;
 	char a[n][l], s[l];
 
-	printf ("Insira %d strings com até %d caracteres.\n\n", n, l-1);
+	printf("Insira %d strings com até %d caracteres.\n\n", n, l - 1);
 
-	for(int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) {
 		gets(s);
 		strcpy(a[i], s);
 	}
 
-	for(int i = 0; i < n-1; i++) {
-		for(int j = i+1; j < n; j++) {
-			if(strcmp(a[i], a[j]) > 0) {
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (strcmp(a[i], a[j]) > 0) {
 				strcpy(s, a[i]);
 				strcpy(a[i], a[j]);
 				strcpy(a[j], s);
@@ -26,7 +32,7 @@ int main (int argc, char *argv[]) {
 	}
 
 	puts("");
-	for(int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) {
 		puts(a[i]);
 	}
 
