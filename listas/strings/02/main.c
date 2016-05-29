a85d6915f9f913d29d2e777f61e40b054873286b
Lucas César <Ahavaz@users.noreply.github.com>
2016-05-29 15:10:48 -0300

    Create main.c

---
+++ listas/strings/02/main.c
@@ -0,0 +1,32 @@
+/*
+ ======================================================================================================================
+ Faça um programa que lê uma string e a imprime invertida.
+ ======================================================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+	setvbuf(stdout, NULL, _IONBF, 0); // comando para imprimir corretamente a saída no console interno do eclipse
+
+	int l = 101, len;
+	char s[l], aux;
+
+	printf("Insira uma string com até %d caracteres.\n\n", l - 1);
+
+	gets(s);
+
+	len = strlen(s);
+	for (int i = 0, j = len - i - 1; i < j; i++, j--) {
+		aux = s[i];
+		s[i] = s[j];
+		s[j] = aux;
+	}
+
+	puts("");
+	puts(s);
+
+	return 0;
+}
