56f1ae7cf2db088b39ed4f0736f145035ca4d0fb
Lucas César <Ahavaz@users.noreply.github.com>
2016-05-29 15:36:23 -0300

    Create main.c

---
+++ listas/strings/03/main.c
@@ -0,0 +1,34 @@
+/*
+ ======================================================================================================================
+ Faça um programa que recebe uma string e um caractere e conte quantas vezes o caractere aparece na string.
+ ======================================================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+	setvbuf(stdout, NULL, _IONBF, 0); // comando para imprimir corretamente a saída no console interno do eclipse
+
+	int l = 101, len, k = 0;
+	char s[l], c;
+
+	printf("Insira uma string com até %d caracteres.\n\n", l - 1);
+
+	gets(s);
+
+	printf("\nInsira um caractere: ");
+	c = getchar();
+
+	len = strlen(s);
+	for (int i = 0; i < len; i++) {
+		if (s[i] == c) {
+			k++;
+		}
+	}
+
+	printf("\nO caractere '%c' aparece %d vezes.", c, k);
+
+	return 0;
+}
