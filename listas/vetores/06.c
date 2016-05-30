8fedf8fabbd0bc647d6299556d291b0f048d84d5
Lucas César <Ahavaz@users.noreply.github.com>
2016-05-30 17:14:14 -0300

    Create 06.c

---
+++ listas/vetores/06.c
@@ -0,0 +1,37 @@
+/*
+ ============================================================================================================================
+ Faça um algoritmo que leia um vetor S[20] e uma variável A. A seguir, mostre o produto da variável A pelo vetor.
+ ============================================================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int t = 20, s[t], a, r[t];
+
+	printf("Insira o valor inteiro da variável A: ");
+	scanf("%d", &a);
+
+	printf("\nInsira os %d valores inteiros do vetor S.\n", t);
+
+	for (int i = 0; i < t; i++) {
+		printf("Posição %d: ", i + 1);
+		scanf("%d", &s[i]);
+
+		r[i] = a * s[i];
+	}
+
+	printf("\nO vetor R resultante do produto de A pelo vetor S é:");
+	printf("\nR: { ");
+
+	for (int i = 0; i < t; i++) {
+		if (i == t - 1) {
+			printf("%d }\n", r[i]);
+			break;
+		}
+		printf("%d, ", r[i]);
+	}
+
+	return 0;
+}
