5702a6fb35e6d486bca1c8e80814392a3556e95d
Lucas César <Ahavaz@users.noreply.github.com>
2016-05-30 01:47:49 -0300

    Update 04.c

--- listas/strings/04.c
+++ listas/strings/04.c
@@ -1,7 +1,7 @@
 /*
- ======================================================================================================================
+ ==========================================================================================================================
  Faça um programa que pede ao usuário para digitar uma string e troque as vogais por '*'.
- ======================================================================================================================
+ ==========================================================================================================================
  */
 
 #include <stdio.h>
@@ -11,32 +11,32 @@
 int main() {
 	int l = 101, len;
 	char s[l], c = '*';
-	
+
 	printf("Insira uma string com até %d caracteres.\n\n", l - 1);
 	gets(s);
 
 	len = strlen(s);
 	for (int i = 0; i < len; i++) {
-		switch ((unsigned char)s[i]) {
-			case 'A':
-			case 'a':
-				s[i] = c;
-				break;
-			case 'E':
-			case 'e':
-				s[i] = c;
-				break;
-			case 'I':
-			case 'i':
-				s[i] = c;
-				break;
-			case 'O':
-			case 'o':
-				s[i] = c;
-				break;
-			case 'U':
-			case 'u':
-				s[i] = c;
+		switch ((unsigned char) s[i]) {
+		case 'A':
+		case 'a':
+			s[i] = c;
+			break;
+		case 'E':
+		case 'e':
+			s[i] = c;
+			break;
+		case 'I':
+		case 'i':
+			s[i] = c;
+			break;
+		case 'O':
+		case 'o':
+			s[i] = c;
+			break;
+		case 'U':
+		case 'u':
+			s[i] = c;
 		}
 	}
 
