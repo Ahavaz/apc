09da8ffd460d118c6ad1b9640b5a7495a3fabef1
Lucas César <Ahavaz@users.noreply.github.com>
2016-05-30 18:55:28 -0300

    Create 10.c

---
+++ listas/vetores/10.c
@@ -0,0 +1,34 @@
+/*
+ ============================================================================================================================
+ Escreva um algoritmo que leia um vetor de 20 posições e mostre-o. Em seguida, troque o primeiro elemento com o último, o
+ segundo com o penúltimo, o terceiro com o antepenúltimo, e assim sucessivamente. Mostre o novo vetor depois da troca.
+ ============================================================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int t = 4, v[t], aux;
+
+	printf("Insira os %d valores inteiros do vetor V.\n", t);
+
+	for (int i = 0; i < t; i++) {
+		printf("Posição %2d: ", i + 1);
+		scanf("%d", &v[i]);
+	}
+
+	for (int i = 0, j = t - 1 - i; i < j; i++, j--) {
+		aux = v[i];
+		v[i] = v[j];
+		v[j] = aux;
+	}
+
+	printf("\nO vetor V em ordem invertida é: { ");
+	for (int i = 0; i < t - 1; i++) {
+		printf("%d, ", v[i]);
+	}
+	printf("%d }\n", v[t - 1]);
+
+	return 0;
+}
