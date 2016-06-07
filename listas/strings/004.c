0dcbb2bd5d452984610f14c61cb2f189eb13f8cd
Lucas César <Ahavaz@users.noreply.github.com>
2016-06-07 09:34:25 -0300

    Create 004.c

---
+++ listas/strings/004.c
@@ -0,0 +1,31 @@
+/*
+ ============================================================================================================================
+ Faça um programa que lê uma string e verifica se ela é palíndrome.
+ ============================================================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+	int l = 101, len;
+	char s[l];
+	_Bool b = 1;
+
+	printf("Insira uma string com até %d caracteres.\n\n", l - 1);
+
+	fgets(s, l, stdin);
+
+	len = strlen(s);
+	for (int i = 0, j = len - i - 1; i < j; i++, j--) {
+		if (s[i] != s[j]) {
+		    b = 0;
+		    break;
+		}
+	}
+
+    b ? printf("A string é palíndrome.") : printf("A string não é palíndrome.");
+    
+	return 0;
+}
