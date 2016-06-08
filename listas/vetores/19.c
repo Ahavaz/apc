01586d68b1b23d59760fe1df4c4be5b38f036873
Lucas César <Ahavaz@users.noreply.github.com>
2016-06-07 23:30:04 -0300

    Create 19.c

---
+++ listas/vetores/19.c
@@ -0,0 +1,8 @@
+/*
+ ============================================================================================================================
+ Faça um algoritmo que leia um vetor A de 100 posições. Em seguida, compacte o vetor, retirando os valores nulos e 
+ negativos, colocando o resultado em um vetor B de 100 posições (deixe em branco as posições não utilizadas).
+ ============================================================================================================================
+ */
+ 
+ 
