eb072c0cdd2ad9b58e14c3b05e815e7eae61a77f
Lucas César <Ahavaz@users.noreply.github.com>
2016-06-07 23:38:55 -0300

    Create 26.c

---
+++ listas/vetores/26.c
@@ -0,0 +1,8 @@
+/*
+ ============================================================================================================================
+ Uma frase/palavra é dita ser palíndrome, se ela é a mesma se lida da esquerda para a direita é a mesma que lida da direita 
+ para a esquerda. Faça um programa que lê uma cadeia de caracteres e diz se a cadeia é palindrome ou não.
+ ============================================================================================================================
+ */
+ 
+ 
