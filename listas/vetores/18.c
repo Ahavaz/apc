732b1e259d8f16b5d22ebb238e80483570c98c49
Lucas César <Ahavaz@users.noreply.github.com>
2016-06-07 23:27:41 -0300

    Create 18.c

---
+++ listas/vetores/18.c
@@ -0,0 +1,8 @@
+/*
+ ============================================================================================================================
+ Escrever um algoritmo que lê um vetor X(20). Escreva, a seguir, cada um dos valores distintos que aparecem em X dizendo 
+ quantas vezes cada valor aparece em X.
+ ============================================================================================================================
+ */
+ 
+ 
