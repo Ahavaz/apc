7b870451d33b9afc7e9c542c2230f7136eff28d9
Lucas César <Ahavaz@users.noreply.github.com>
2016-06-07 23:40:14 -0300

    Create 27.c

---
+++ listas/vetores/27.c
@@ -0,0 +1,12 @@
+/*
+ ============================================================================================================================
+ O imperador romano Júlio César usava um método de criptografia para mandar mensagens criptografadas para seus generais. 
+ Este método considerava a ordem dos caracteres no alfabeto e trocava cada letra pela k-ésima letra seguinte a letra. Por 
+ exemplo, se k = 3, então a ocorrência da letra 'A' era trocada pela letra 'D'. A letra 'B' era trocada pela letra 'E', assim
+ por diante. Esta substituição era feita de maneira cíclica, que neste caso a letra 'Z' era trocada pela letra 'C'. Faça um 
+ programa que lê um inteiro k e uma cadeia de caracteres e imprime a correspondente mensagem criptografada pela cifra de 
+ César com parâmetro k.
+ ============================================================================================================================
+ */
+ 
+ 
