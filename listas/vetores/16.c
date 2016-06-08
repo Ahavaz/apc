d5aeefc0fd39331e738a27ca4cc57df771eb0f5f
Lucas César <Ahavaz@users.noreply.github.com>
2016-06-07 23:26:11 -0300

    Create 16.c

---
+++ listas/vetores/16.c
@@ -0,0 +1,11 @@
+/*
+ ============================================================================================================================
+ Escrever um algoritmo que lê 2 vetores X(10) e Y(10). Crie, a seguir, um vetor Z que seja:
+ a) a diferença entre X e Y;
+ b) a soma entre X e Y;
+ c) o produto entre X e Y.
+ Escreva o vetor Z a cada cálculo.
+ ============================================================================================================================
+ */
+ 
+ 
