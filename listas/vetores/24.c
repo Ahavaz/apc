09eebdf845b52569ee4661a9afa3a11b978748c3
Lucas César <Ahavaz@users.noreply.github.com>
2016-06-07 23:36:37 -0300

    Create 24.c

---
+++ listas/vetores/24.c
@@ -0,0 +1,8 @@
+/*
+ ============================================================================================================================
+ Faça um algoritmo que leia um vetor A[10]. Preencha então um vetor B[10] com o fatorial de cada valor de A respeitando as 
+ posições, caso o referido valor for positivo ou nulo. Deixe os valores negativos intactos. Mostre o vetor B.
+ ============================================================================================================================
+ */
+ 
+ 
