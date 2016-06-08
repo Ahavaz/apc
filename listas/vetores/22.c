c6ac7aa96eea756334d112a881714de6fe68865f
Lucas César <Ahavaz@users.noreply.github.com>
2016-06-07 23:33:36 -0300

    Update 22.c

--- listas/vetores/22.c
+++ listas/vetores/22.c
@@ -1,4 +1,9 @@
-Faça um algoritmo que leia dois vetores (A e B) de 50 números. O algoritmo deve,
-então, subtrair o primeiro elemento de A do último de B, acumulando o valor, subtrair o
-segundo elemento de A do penúltimo de B, acumulando o valor, e assim por diante.
-Mostre o resultado da soma final.
+/*
+ ============================================================================================================================
+ Faça um algoritmo que leia dois vetores (A e B) de 50 números. O algoritmo deve, então, subtrair o primeiro elemento de A 
+ do último de B, acumulando o valor, subtrair o segundo elemento de A do penúltimo de B, acumulando o valor, e assim por 
+ diante. Mostre o resultado da soma final.
+ ============================================================================================================================
+ */
+ 
+ 
