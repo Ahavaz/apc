fa427a31244c3b42cc39ef65d05a0ded36aefbc9
Lucas César <Ahavaz@users.noreply.github.com>
2016-06-08 01:16:04 -0300

    Update 16.c

--- listas/vetores/16.c
+++ listas/vetores/16.c
@@ -7,5 +7,63 @@
  Escreva o vetor Z a cada cálculo.
  ============================================================================================================================
  */
- 
- 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+	int t = 10, x[t], y[t], z[t];
+
+	printf("Insira os %d valores inteiros do vetor X.\n", t);
+
+	for (int i = 0; i < t; i++) {
+		printf("Posição %2d: ", i + 1);
+		scanf("%d", &x[i]);
+	}
+
+	printf("\nInsira os %d valores inteiros do vetor Y.\n", t);
+
+	for (int i = 0; i < t; i++) {
+		printf("Posição %2d: ", i + 1);
+		scanf("%d", &y[i]);
+	}
+
+	// Diferença
+	for (int i = 0; i < t; i++) {
+		z[i] = x[i] - y[i];
+	}
+
+	int i = -1;
+	printf("\nX - Y = { ");
+	while (++i < t - 1) {
+		printf("%d, ", z[i]);
+	}
+	printf("%d }\n", z[i]);
+
+	// Soma
+	for (int i = 0; i < t; i++) {
+		z[i] = x[i] + y[i];
+	}
+
+	i = -1;
+	printf("\nX + Y = { ");
+	while (++i < t - 1) {
+		printf("%d, ", z[i]);
+	}
+	printf("%d }\n", z[i]);
+
+	// Produto
+	for (int i = 0; i < t; i++) {
+		z[i] = x[i] * y[i];
+	}
+
+	i = -1;
+	printf("\nX * Y = { ");
+	while (++i < t - 1) {
+		printf("%d, ", z[i]);
+	}
+	printf("%d }\n", z[i]);
+
+	return 0;
+}
