89d958e1053aee0404d3f54b72b0fa9ce811bfc1
Lucas César <Ahavaz@users.noreply.github.com>
2016-06-08 21:10:55 -0300

    Update 25.c

--- listas/vetores/25.c
+++ listas/vetores/25.c
@@ -1,8 +1,34 @@
 /*
  ============================================================================================================================
- Faça um programa que lê uma cadeia de caracteres e transforma a cadeia de caracteres na mesma cadeia, mas com as letras 
+ Faça um programa que lê uma cadeia de caracteres e transforma a cadeia de caracteres na mesma cadeia, mas com as letras
  transformadas para minúsculas. Em seguida imprime a cadeia de caracteres resultante.
  ============================================================================================================================
  */
- 
- 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int t = 100, l = 0;
+	char s[t], c;
+
+	printf("Insira uma string com até %d caracteres.\n", t);
+
+	while (l < t) {
+		c = getchar();
+		if (c == '\n') {
+			break;
+		}
+		if (64 < c && c < 91) {
+			c += 32;
+		}
+		s[l++] = c;
+	}
+
+	int i = -1;
+	while (++i < l) {
+		printf("%c", s[i]);
+	}
+
+	return 0;
+}
