39ec4a4445172cebca34f4e3e1d2a59de2dce60a
Lucas César <Ahavaz@users.noreply.github.com>
2016-06-09 14:01:10 -0300

    Update 25.c

--- listas/vetores/25.c
+++ listas/vetores/25.c
@@ -13,22 +13,19 @@ int main() {
 	char s[t], c;
 
 	printf("Insira uma string com até %d caracteres.\n", t);
-
-	while (l < t) {
-		c = getchar();
-		if (c == '\n') {
-			break;
-		}
+	fgets(s, t, stdin);
+	
+	len = strlen(s);
+	while (l < len) {
+		c = s[l];
 		if (64 < c && c < 91) {
 			c += 32;
 		}
 		s[l++] = c;
 	}
 
-	int i = -1;
-	while (++i < l) {
-		printf("%c", s[i]);
-	}
+	puts("");
+	puts(s);
 
 	return 0;
 }
