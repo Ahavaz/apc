9187369ad4943b9e6fe2a5a290c58bff5358131b
Lucas César <Ahavaz@users.noreply.github.com>
2016-06-10 20:46:12 -0300

    Create 01.c

---
+++ listas/matrizes/01.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+	setvbuf(stdout, NULL, _IONBF, 0);
+	int m = 11, n = m, a[m][n], k = 0;
+
+	while (k < m) {
+		for (int i = k; i + k < m; i++) {
+			for (int j = k; j + k < n; j++) {
+				a[i][j] = k + 1;
+			}
+		}
+		k++;
+	}
+
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%3d ", a[i][j]);
+		}
+		puts("\n");
+	}
+
+	return 0;
+}
