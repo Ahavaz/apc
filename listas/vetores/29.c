694d04471e1bbb291703c9e96b55ad290c0c2e94
Lucas César <Ahavaz@users.noreply.github.com>
2016-06-10 18:16:02 -0300

    Update 29.c

--- listas/vetores/29.c
+++ listas/vetores/29.c
@@ -6,25 +6,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int main() {
 	int n, k = 0;
 
-	printf("Insira o valor de N: ");
-	scanf("%d", &n);
+	do {
+		printf("Insira o valor de N (menor que 35): ");
+		fflush (stdin);
+		scanf("%d", &n);
+	} while (n > 34);
 
-	long t = n * (n + 1) / 2, p[t];
+	int t = n * (n + 1) / 2, p[t];
 
 	puts("\n\n");
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n - i; j++) {
 			if (i == 0 || j == 0) {
 				p[k] = 1;
-				printf("%10ld ", p[k++]);
+				printf("%10d ", p[k++]);
 			} else {
 				p[k] = p[k - 1] + p[k - 1 - n + i];
-				printf("%10ld ", p[k++]);
+				printf("%10d ", p[k++]);
 			}
 		}
 		puts("\n\n\n\n");
