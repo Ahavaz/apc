16e8f3f062b46da085335f660fae0dbf8ef1b047
Lucas César <Ahavaz@users.noreply.github.com>
2016-06-14 16:00:35 -0300

    Update string.c

--- aulas/string.c
+++ aulas/string.c
@@ -1,16 +1,15 @@
 /*
- ================================================================================================
- Faça um programa que leia 2 strings (A e B) e retorne uma terceira string (C) formada
- pelos caracteres de A e B intercalados. Ex.: Se A='Quarta' e B='Segunda', a resposta
- deve ser 'QSueagrutnada'.
- ================================================================================================
+ ======================================================================================================================
+ Faça um programa que leia 2 strings (A e B) e retorne uma terceira string (C) formada pelos caracteres de A e B
+ intercalados. Ex.: Se A='Quarta' e B='Segunda', a resposta deve ser 'QSueagrutnada'.
+ ======================================================================================================================
  */
 
 #include <stdio.h>
 #include <string.h>
 
 int main() {
-	int t = 101, k = 0, len1, len2, len3, i;
+	int t = 101, k = 0, len1, len2, len3;
 	char s1[t], s2[t], s3[2 * t];
 
 	printf("Digite a string A (máx %d caracteres): ", t - 1);
@@ -24,7 +23,7 @@ int main() {
 	len2 = strlen(s2) - 1;
 	len3 = len1 + len2;
 
-	for (i = 0; i < len3; i++) {
+	for (int i = 0; i < len3; i++) {
 		if (i < len1) {
 			s3[k++] = s1[i];
 		}
