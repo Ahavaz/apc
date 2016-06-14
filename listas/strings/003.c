cc39fba031bf04c572e6e5481fa210ab86862915
Lucas César <Ahavaz@users.noreply.github.com>
2016-06-14 16:25:29 -0300

    Update 003.c

--- listas/strings/003.c
+++ listas/strings/003.c
@@ -5,7 +5,6 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 int main() {
@@ -17,15 +16,15 @@ int main() {
 
 	fgets(s, l, stdin);
 
-	len = strlen(s);
+	len = strlen(s) - 1;
 	for (int i = 0, j = len - i - 1; i < j; i++, j--) {
 		if (s[i] != s[j]) {
-		    b = 0;
-		    break;
+			b = 0;
+			break;
 		}
 	}
 
-    b ? printf("A string é palíndrome.") : printf("A string não é palíndrome.");
-    
+	b ? printf("A string é palíndrome.") : printf("A string não é palíndrome.");
+
 	return 0;
 }
