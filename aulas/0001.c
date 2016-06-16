a2fdcedd78866c67001be505a5ca2f17e71fcc8a
Lucas César <Ahavaz@users.noreply.github.com>
2016-06-16 09:38:25 -0300

    Update 0001.c

--- aulas/0001.c
+++ aulas/0001.c
@@ -11,30 +11,73 @@
 #include <stdio.h>
 
 int main() {
-	int m, n, k = 0;
-
-	printf("Digite o número de linhas: ");
+	int m, i, j, t, k = 0;
+	_Bool b = 1;
+    
+	printf("Digite o tamanho da matriz M: ");
 	scanf("%d", &m);
-	printf("Digite o número de colunas: ");
-	scanf("%d", &n);
-	int a[m][n];
-
-	while (k < m) {
-		for (int i = k; i + k < m; i++) {
-			for (int j = k; j + k < n; j++) {
-				a[i][j] = k + 1;
-			}
+	t = 2 * m + 2;
+	int a[m][m], s[t];
+    
+    // soma das linhas
+	for (i = 0; i < m; i++) {
+        s[k] = 0;
+		for (j = 0; j < m; j++) {
+            printf("Digite o valor da posição M[%d][%d]: ", i, j);
+            scanf("%d", &a[i][j]);
+            
+            s[k] += a[i][j];
 		}
 		k++;
 	}
-
-	puts("");
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++) {
+    
+    // soma das colunas
+    for (i = 0; i < m; i++) {
+        s[k] = 0;
+        for (j = 0; j < m; j++) {
+            s[k] += a[j][i];
+        }
+        k++;
+    }
+    
+    // soma da diagonal principal
+    for (i = 0; i < m; i++) {
+        s[k] = 0;
+        for (j = 0; j < m; j++) {
+            if (i == j) {
+                s[k] += a[i][j];
+            }
+        }
+    }
+    k++;
+    
+    // soma da diagonal secundária
+    for (i = 0; i < m; i++) {
+        s[k] = 0;
+        for (j = 0; j < m; j++) {
+            if (i + j == m - 1) {
+                s[k] += a[i][j];
+            }
+        }
+    }
+    k++;
+    
+	puts("\nA matriz M: \n");
+	for (i = 0; i < m; i++) {
+		for (j = 0; j < m; j++) {
 			printf("%3d ", a[i][j]);
 		}
 		puts("\n");
 	}
+	
+    while (k-- > 1) {
+        printf("%d", s[k]);
+        if (s[k] != s[k - 1]) {
+            b = 0;
+        }
+    }
+    
+    b ? puts("É um quadrado mágico.") : puts("Não é um quadrado mágico.");
 
 	return 0;
 }
