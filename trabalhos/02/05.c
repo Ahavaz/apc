b2af2eb8591d9953df8c4e83e7c371a41df36505
Lucas César <Ahavaz@users.noreply.github.com>
2016-06-17 02:40:44 -0300

    Update 05.c

--- trabalhos/02/05.c
+++ trabalhos/02/05.c
@@ -1,3 +1,37 @@
-Escreva um programa em C que cria um vetor V de 20 números inteiros. Solicite ao
-usuário que informe esses números. Imprima a média dos elementos desse vetor.
-Em seguida, informe quantos elementos estão abaixo dessa média.
+/*
+======================================================================================================================
+Escreva um programa em C que cria um vetor V de 20 números inteiros. Solicite ao usuário que informe esses números.
+Imprima a média dos elementos desse vetor. Em seguida, informe quantos elementos estão abaixo dessa média.
+======================================================================================================================
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "");
+
+	int t = 20, v[20], i, soma = 0, k = 0;
+	float media;
+
+	printf("Insira os %d valores inteiros do vetor V.\n\n", t);
+
+	for (i = 0; i < t; i++) {
+		printf("V[%d]: ", i);
+		scanf("%d", &v[i]);
+		soma += v[i];
+	}
+
+	media = (float)soma / t;
+	printf("\nA média é %.2f\n", media);
+
+	for (i = 0; i < t; i++) {
+		if (v[i] < media) {
+			k++;
+		}
+	}
+	printf("\nHá %d valor(es) abaixo da média.\n\n", k);
+
+	return 0;
+}
