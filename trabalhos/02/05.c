c1faae29bbabb969fefad010bbc0bcdb64e00645
Lucas César <Ahavaz@users.noreply.github.com>
2016-06-17 01:39:10 -0300

    Create 05.c

---
+++ trabalhos/02/05.c
@@ -0,0 +1,3 @@
+Escreva um programa em C que cria um vetor V de 20 números inteiros. Solicite ao
+usuário que informe esses números. Imprima a média dos elementos desse vetor.
+Em seguida, informe quantos elementos estão abaixo dessa média.
