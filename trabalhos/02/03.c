f4f3fdb5793c4e18312f4c28a54d86fa8eaa6df8
Lucas César <Ahavaz@users.noreply.github.com>
2016-06-17 01:17:20 -0300

    Update 03.c

--- trabalhos/02/03.c
+++ trabalhos/02/03.c
@@ -1,1 +1,38 @@
+/*
+======================================================================================================================
+Escreva um programa em C que lê uma matriz de inteiros M(6,6). Conte e exiba a quantidade de números maiores do que 10
+e pares existentes na matriz.
+======================================================================================================================
+*/
 
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "");
+	int i, j, k = 0, l = 6, m[6][6];
+
+	printf("Insira os valores da matriz M de tamanho %d.\n\n", l);
+
+	for (i = 0; i < l; i++) {
+		for (j = 0; j < l; j++) {
+			printf("Digite o valor da posição M[%d][%d]: ", i, j);
+			scanf("%d", &m[i][j]);
+
+			if(m[i][j] > 10 && m[i][j] % 2 == 0)
+				k++;
+		}
+	}
+
+	puts("\n");
+	for (i = 0; i < l; i++) {
+		for (j = 0; j < l; j++) {
+			printf("%3d ", m[i][j]);
+		}
+		puts("\n");
+	}
+
+	printf("\nA matriz M possui %d valor(es) maior(es) do que 10 e par(es).\n\n", k);
+
+	return 0;
+}
